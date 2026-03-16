5a31bd3a94df1fcb9ccfce0535105e65f25e8028
Arian-Bn <arianbagheri2476@gmail.com>
2026-03-16 05:03:04 +0000

    Add client implementation: connects to server on port 9000 and sends test message

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,39 @@
+#include <array>
+#include <boost/asio.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/registered_buffer.hpp>
 #include <iostream>
 
-int main() { return 0; }
+int main() {
+  try {
+    std::cout << "Client starting..." << std::endl;
+
+    boost::asio::io_context io_context;
+
+    boost::asio::ip::tcp::socket socket(io_context);
+    boost::asio::ip::tcp::resolver resolver(io_context);
+    auto endpoint = resolver.resolve("127.0.0.1", "9000");
+
+    boost::asio::connect(socket, endpoint);
+    std::cout << "Connected to server!" << std::endl;
+
+    std::string message = "Hello from client1";
+    boost::asio::write(socket, boost::asio::buffer(message));
+    std::cout << "Sent: " << message << std::endl;
+
+    std::array<char, 128> buf;
+    boost::system::error_code error;
+    size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+    if (!error) {
+      std::cout << "Received: " << std::string(buf.data(), len) << std::endl;
+    }
+
+  } catch (const std::exception &e) {
+    std::cerr << "Errro: " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
